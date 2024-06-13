41d1f882a769f3ef9717d6f5817480ae179c3379
Teyeye6464 <eldzej398@gmail.com>
2024-06-13 09:45:20 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+const startExchangeButton = document.getElementById('start-exchange');
+const learnMoreButton = document.getElementById('learn-more');
+const exchangeCurrenciesButton = document.getElementById('exchange-currencies');
+
+startExchangeButton.addEventListener('click', () => {
+    // Add functionality for starting the exchange
+});
+
+learnMoreButton.addEventListener('click', () => {
+    // Add functionality for learning more
+});
+
+exchangeCurrenciesButton.addEventListener('click', () => {
+    // Add functionality for exchanging currencies
+});
